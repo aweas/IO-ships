84aab3f1112745d6ad016843dbaca804620f3179
wwydm <wwydmanski@gmail.com>
2018-04-12 18:39:49 +0200

    Step by step to victory

--- IOships/MainWindow.xaml.cs
+++ IOships/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using LiveCharts;
 using LiveCharts.Wpf;
 using LiveCharts.Defaults;
+using System.ComponentModel;
 
 namespace IOships
 {
@@ -30,7 +31,7 @@ namespace IOships
             {
                 new PieSeries
                 {
-                    Title = "Newly arrived",
+                    Title = "Newly arrived containers",
                     Values = new ChartValues<ObservableValue> { new ObservableValue(250) },
                     DataLabels = true
                 },
@@ -42,9 +43,79 @@ namespace IOships
                 }
             };
 
+            Ship ship0 = new Ship(0, shipDataGrid);
+            Ship ship1 = new Ship(1, shipDataGrid);
+
             DataContext = this;
         }
 
         public SeriesCollection SeriesCollection { get; set; }
+        public SeriesCollection Ship1Data { get; set; }
+        public SeriesCollection Ship2Data { get; set; }
+        public string ship1text { get; set; }
+    }
+
+    public class Ship : INotifyPropertyChanged
+    {
+        public CartesianChart ship;
+        public Label average;
+        public SeriesCollection shipData { get; set; }
+        public String avg { get; set; }
+
+        public Ship(int id, Grid shipDataGrid)
+        {
+            setupLabel(id, shipDataGrid);
+            setupPlot(id, shipDataGrid);
+        }
+
+        private void setupPlot(int id, Grid shipDataGrid)
+        {
+            shipData = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<int>{86, 43, 66, 43, 75 }
+                }
+            };
+
+            ship = new CartesianChart();
+            shipDataGrid.Children.Add(ship);
+            Grid.SetRow(ship, id + 1);
+            Grid.SetColumn(ship, 1);
+
+            Binding dataBinding = new Binding("shipData");
+            dataBinding.Source = this;
+
+            ship.SetBinding(CartesianChart.SeriesProperty, dataBinding);
+        }
+
+        private void setupLabel(int id, Grid shipDataGrid)
+        {
+            avg = "dupa";
+            average = new Label();
+            average.FontFamily = new System.Windows.Media.FontFamily("Segoe UI Semibold");
+            average.Foreground = new SolidColorBrush(Colors.White);
+            average.VerticalAlignment = VerticalAlignment.Bottom;
+            average.HorizontalAlignment = HorizontalAlignment.Center;
+            average.FontSize = 24;
+            shipDataGrid.Children.Add(average);
+            Grid.SetRow(average, id + 1);
+            Grid.SetColumn(average, 2);
+
+            Binding textBinding = new Binding("avg");
+            textBinding.Source = this;
+
+            average.SetBinding(Label.ContentProperty, textBinding);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
