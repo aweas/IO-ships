b52c016058aa153d5b1e81d8b3c31a7a12a3c8a4
wwydm <wwydmanski@gmail.com>
2018-04-12 14:42:56 +0200

    Dodaj pliki projektów.

---
+++ IOships/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Defaults;
+
+namespace IOships
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SeriesCollection = new SeriesCollection
+            {
+                new PieSeries
+                {
+                    Title = "Newly arrived",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(250) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Waiting for 1 round",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(130) },
+                    DataLabels = true
+                }
+            };
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+    }
+}
